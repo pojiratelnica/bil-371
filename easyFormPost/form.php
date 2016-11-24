d44d038e3b0150cf33c73d5b714cba94069e48d0
pojiratelnica <pojiratelnica@gmail.com>
2016-11-24 11:37:33 +0600

    before mvc

--- easyFormPost/form.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head><title>Form</title></head>
-<body>
-
-Welcome <b> <?php echo $_POST["name"]; ?></b><br>
-Your email address is: <b><?php echo $_POST["email"]; ?></b>
-
-</body>
-</html>
